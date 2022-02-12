b162d3a7feb78e94d31dd75cd557a131a60a683f
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-12 06:59:05 -0800

    event fun

--- banking.js
+++ banking.js
@@ -91,3 +91,69 @@ function updateBalance(amount, isAdd) {
         balanceTotal.innerText = previousBalanceTotal - amount;
     }
 }
+
+
+/////event function
+document.getElementById('deposit-button').addEventListener('click', function () {
+    /* 
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText); */
+
+
+    // get and update deposit total
+    /*  
+    const depositTotal = document.getElementById('deposit-total');
+     const depositTotalText = depositTotal.innerText;
+     const previousDepositTotal = parseFloat(depositTotalText);
+ 
+     depositTotal.innerText = previousDepositTotal + depositAmount; */
+
+
+    // update balance
+    /* 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount; */
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+});
+
+// handle withdraw button 
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    /*     
+    const withdrawInput = document.getElementById('withdraw-input');
+        const withdrawAmountText = withdrawInput.value;
+        const withdrawAmount = parseFloat(withdrawAmountText); */
+
+
+    // get and update withdraw total
+    /* 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount; */
+
+
+    // update balance after withdraw
+    /* 
+        const balanceTotal = document.getElementById('balance-total');
+        const balanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+    
+        balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
+
+    const withdrawAmount = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance) {
+        console.log('You can not withdraw more than what you have in your account');
+    }
+});
